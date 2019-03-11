3347410e882c23d5ec2e4a8715fbbf37605d4dbe
Burak Ekmen <ekmen.burak@hotmail.com>
2019-03-11 14:12:33 +0300

    SQLite veritabanı entegrasyonu tamamlandı. Favoriler sayfası için istek hazırlanıyor.

--- app/src/main/java/com/burakekmen/rickandmortyguide/network/ApiInterface.kt
+++ app/src/main/java/com/burakekmen/rickandmortyguide/network/ApiInterface.kt
@@ -1,7 +1,6 @@
 package com.burakekmen.rickandmortyguide.network
 
-import com.burakekmen.rickandmortyguide.model.Location
-import com.burakekmen.rickandmortyguide.model.Result
+import com.burakekmen.rickandmortyguide.model.CharacterResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,9 +8,20 @@ import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @GET("character/")
-    fun getCharacterPage(@Query("page") page: Int?): Call<MutableList<Result>>
 
-    @GET("location/{locationId}")
-    fun getLocationDetail(@Path("locationId") locationId: Int?): Call<Location>
+    @GET("character/")
+    fun getCharacterPage(@Query("page") page: Int?): Call<CharacterResponse>
+
+    @GET("character/{characterIds}")
+    fun getFavouriteCharacters(@Path("characterIds") characterIds: String?): Call<CharacterResponse>
+
+    @GET("character/")
+    fun getCharacterSearch(@Query("page") page: Int?, @Query("name") name: String?): Call<CharacterResponse>
+
+    @GET("character/")
+    fun getCharacterSearchWithStatus(@Query("page") page: Int?, @Query("status") status: String?): Call<CharacterResponse>
+
+    @GET("character/")
+    fun getCharacterSearchWithNameAndStatus(@Query("page") page: Int?, @Query("name") name: String?, @Query("status") status: String?): Call<CharacterResponse>
+
 }
